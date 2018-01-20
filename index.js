2ace9b9d12d3aabec1454829a03a07e2091c0113
Rebecca Leff <rebeccacleff@gmail.com>
2018-01-20 20:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var animal = 'dog'
 
 function myAnimal() {
-  return animal
+  var animal = 'dog'
+  return var animal
 }
-
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+  function yourAnimal() {
+    // How can we make sure that this function
+    // and the above function both pass?
+    // P.S.: You can't just hard-code 'cat' below
+    var animal = 'cat'
+    return var animal
 }
 
 function add2(n) {
